fda07a80ed71a96992439bbd9f34b83db378cef6
AlinCqe <alinemanuelv@gmail.com>
2026-01-03 18:33:59 +0200

    Improve error handling and minor cleanup in image scraper and BaseLinker API

--- app/core.py
+++ app/core.py
@@ -5,13 +5,14 @@ from dotenv import load_dotenv
 import os
 from bs4 import BeautifulSoup
 import logging
+from fastapi import HTTPException
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 load_dotenv()
 
 XBLToken = os.getenv("XBLToken")
-headers ={"X-BLToken": XBLToken}
+XBL_HEADERS  ={"X-BLToken": XBLToken}
 
 SCRAPING_HEADERS = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
@@ -31,10 +32,11 @@ def get_link_from_sku(sku: str) -> str:
 
     for row in data:
         if row["sku"] == sku:
-            logging.info(f"Found link for sku: {sku}; {row["link"]}")
+            logging.info(f"Found link for sku: {sku}; {row['link']}")
             return row["link"]
+        
     logging.error(f"Unable to find link for sku: {sku}")
-    return None
+    raise HTTPException(404, f"Unable to find link for sku: {sku}")
 
 
 
@@ -54,13 +56,19 @@ def update_images_base(inventory_id: str, item_id: str, images: dict):
         response = requests.post(
             "https://api.baselinker.com/connector.php", 
             data=payload,
-            headers=headers)
+            headers=XBL_HEADERS,  
+            timeout=14)
         
-    except Exception as e:
+    except requests.Timeout:
+        logging.error("BaseLinker POST request timed out")
+        raise HTTPException(504, "Upstream server took too long to respond")
+    
+    except requests.RequestException as e:
+
         logging.error(f"Something when wrong with the post call to Base.\n{e}")
-
-    return [response]
-
+        raise HTTPException(502, f"Failed to update BaseLinker. {e}")
+    
+    return response
 
 
 def get_imgs_from_link(url):        
@@ -70,20 +78,31 @@ def get_imgs_from_link(url):
     formated_links={}
 
     try:
-        response = session.get(url, headers=SCRAPING_HEADERS)
-        logging.info(f"Headres: {SCRAPING_HEADERS}")
+        response = session.get(url,  timeout=14)
+        logging.info(f"Headers: {SCRAPING_HEADERS}")
 
-    except Exception as e:
-        logging.error(f"Something went wrong with the requesto to {url}:\n{e}")
-        return None
+    except requests.Timeout:
+        logging.error("Emag GET request timed out")
+        raise HTTPException(504, "Upstream server took too long to respond")
     
+    except requests.RequestException as e:
+        logging.error(f"Something went wrong with the requesto to {url}:\n{e}")
+        raise HTTPException(status_code=502,
+            detail=f"Something went wrong with the requesto to {url}:\n{e}")
+
+
     if response.status_code == 200:
         logging.info(f"Request made, Status code 200")
 
         soup = BeautifulSoup(response.text, 'html.parser')
 
         wraper = soup.find("div", class_="multimedia-gallery")
+        if not wraper:
+            raise HTTPException(502, "multimedia-gallery div not found")
+
         imgs = wraper.find_all("div", class_="thumbnail-wrapper")
+        if not imgs:
+            raise HTTPException(502, "No thumbnail-wrapper elements found")
 
         for img in imgs:
             a_tag = img.find("a")
@@ -91,8 +110,11 @@ def get_imgs_from_link(url):
             if a_tag and "href" in a_tag.attrs:
                 link = a_tag["href"]
                 links.append(link)
+
     else:
         logging.error(f"Something went wrong with the requesto to {url}.\nStatus code {response.status_code}. Error mesage: {response.text}")
+        raise HTTPException(status_code=502,
+            detail=f"Something went wrong with the requesto to {url}.\nStatus code {response.status_code}. Error mesage: {response.text}")
 
     if len(links) >= 1:    
         logging.info(f"Found a total of {len(links)} images: {links}")
@@ -101,6 +123,6 @@ def get_imgs_from_link(url):
         return formated_links
     else:
         logging.error(f"Found images are less than 1. {links}")
-        return None
-
+        raise HTTPException(status_code=502,
+            detail=f"Found images are less than 1. {links}")
 
