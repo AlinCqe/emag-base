ae38b33ac864460a3c8850fe6c7efb4a388c7f16
AlinCqe <alinemanuelv@gmail.com>
2026-01-06 00:04:39 +0200

    allow head requests

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from pydantic import BaseModel
 from .core import get_imgs_from_link, update_images_base, get_link_from_sku
 import logging
@@ -14,18 +14,16 @@ class ItemRequest(BaseModel):
     inventory_id: int
 
 
+@app.head("/UpdateImages")
+def update_images():
+    return Response(status_code=200)
+
+
 @app.get("/UpdateImages")
 def update_images(id:str, sku:str, inventory_id:str):
     logger.info(f"Received item: {sku}")
-    print(id)
     link = get_link_from_sku(sku)
-    print(link)
-    if not link:
-        raise HTTPException(
-            status_code=404,
-            detail=f"No link found for item {sku}"
-        )
-    
+
     logger.info(f"Link: {link}")
     imgs = get_imgs_from_link(link)
 
@@ -36,5 +34,7 @@ def update_images(id:str, sku:str, inventory_id:str):
         )
     
     logger.info(f"Images: {imgs}")
-    update_images_base(inventory_id=inventory_id, item_id=id, images=imgs)
-    return {"status": "ok"}
+    response = update_images_base(inventory_id=inventory_id, item_id=id, images=imgs)
+    if response.status_code == 200:
+        return {"status": "ok"}
+    
